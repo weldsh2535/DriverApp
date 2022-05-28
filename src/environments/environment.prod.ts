76c2b4f1c4061af25b566fd2def3c866b929b5b6
weldsh <weldetsadik2535@gmail.com>
2022-05-28 04:05:03 -0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   //apiURL: 'http://localhost:8088/api'
-  apiURL: 'http://hdapi.mtksolutions.com/api'
+  //apiURL: 'http://hdapi.mtksolutions.com/api'
+  apiURL: 'http://localhost:49347/api'
 };
